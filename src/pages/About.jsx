ca93bb022eb3f5f1d8e429ed56b309d4e937f935
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-07 09:51:09 +0530

    #

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,6 +8,7 @@ import FoundingStory from "../assets/Images/FoundingStory.png";
 import StatsComponent from "../components/core/AboutPage/StatsComponent";
 import LearningGrid from "../components/core/AboutPage/LearningGrid";
 import ContactFormSection from "../components/core/AboutPage/ContactFormSection";
+import Footer from "../components/common/Footer";
 
 const About = () => {
   return (
@@ -44,49 +45,87 @@ const About = () => {
       <section>
         <div className="flex flex-col">
           {/* top box */}
-          <div  className="flex">
-              {/* left vala box*/}
-              <div>
-                <h1>Our Founding Story</h1>
-                <p>Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.</p>
-                <p>As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.</p>
-              </div>
+          <div className="flex">
+            {/* left vala box*/}
+            <div>
+              <h1>Our Founding Story</h1>
+              <p>
+                Our e-learning platform was born out of a shared vision and
+                passion for transforming education. It all began with a group of
+                educators, technologists, and lifelong learners who recognized
+                the need for accessible, flexible, and high-quality learning
+                opportunities in a rapidly evolving digital world.
+              </p>
+              <p>
+                As experienced educators ourselves, we witnessed firsthand the
+                limitations and challenges of traditional education systems. We
+                believed that education should not be confined to the walls of a
+                classroom or restricted by geographical boundaries. We
+                envisioned a platform that could bridge these gaps and empower
+                individuals from all walks of life to unlock their full
+                potential.
+              </p>
+            </div>
 
-              {/* right box from top box */}
-              <div>
-                <img src={FoundingStory} alt="" />
-              </div>
+            {/* right box from top box */}
+            <div>
+              <img src={FoundingStory} alt="" />
+            </div>
           </div>
           {/* Bottom box */}
           <div className="flex">
             {/* left box */}
             <div>
               <h1>Our Vision</h1>
-              <p>With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.</p>
+              <p>
+                With this vision in mind, we set out on a journey to create an
+                e-learning platform that would revolutionize the way people
+                learn. Our team of dedicated experts worked tirelessly to
+                develop a robust and intuitive platform that combines
+                cutting-edge technology with engaging content, fostering a
+                dynamic and interactive learning experience.
+              </p>
             </div>
 
             {/* right box */}
             <div>
               <h1>Our Mission</h1>
-              <p>our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.</p>
+              <p>
+                our mission goes beyond just delivering courses online. We
+                wanted to create a vibrant community of learners, where
+                individuals can connect, collaborate, and learn from one
+                another. We believe that knowledge thrives in an environment of
+                sharing and dialogue, and we foster this spirit of collaboration
+                through forums, live sessions, and networking opportunities.
+              </p>
             </div>
           </div>
         </div>
       </section>
 
-
       {/* section - 4 */}
       <section>
-        <StatsComponent/>
+        <StatsComponent />
       </section>
-
 
       {/* section - 5 */}
       <section className="flex items-center flex-col justify-between gap-5 mb-150px]">
-        <LearningGrid/>
-        <ContactFormSection/>
+        <LearningGrid />
+        <ContactFormSection />
       </section>
 
+      {/* section - 6  */}
+      <section>
+        <div>
+          Reviews from other learners
+          {/* <ReviewSlider/> */}
+        </div>
+      </section>
+
+      {/* section - 7 */}
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
